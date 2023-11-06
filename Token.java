23399638151b13de75f6bf85d155698e517673e1
Manoj Gurung <manojgurungpi@gmail.com>
2023-11-06 00:19:06 -0600

    Added Scanner for getting tokens from the source code

--- Token.java
+++ Token.java
@@ -4,7 +4,7 @@ class Token {
     final Object literal;
     final int line;
 
-    Token(TokenType type; String lexeme; Object literal, int line) {
+    Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
         this.lexeme = lexeme;
         this.literal = literal;
